ca002f62dd302084c3192185e2b532baf6e04bad
Philip Nys <archsyker@gmail.com>
2024-07-26 10:25:37 +0200

    init ex2

---
+++ SS21_Uebung2/src/exercise2/test/AddressBookModelMockUp.java
@@ -0,0 +1,32 @@
+/*
+ * Example code used in exercises for lecture "Grundlagen des Software-Testens"
+ * Created and given by Ina Schieferdecker, Theo Vassiliou and Diana Serbanescu
+ * Technische Universität Berlin
+ */
+package exercise2.test;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import exercise2.addressbook.model.AddressBookModel;
+import exercise2.addressbook.model.Entry;
+import exercise2.addressbook.model.SizeLimitReachedException;
+
+/**
+ * Uebung 2 - Komponenten und Integrationstest
+ * Mock-Up für das AddressBookModel
+ * 
+ * Bitte Nummer der Gruppe eintragen:
+ * 0
+ * 
+ * Bitte Gruppenmitglieder eintragen:
+ * @author ...
+ * @author ...
+ * @author ...
+ * @author ...
+ */
+public class AddressBookModelMockUp implements AddressBookModel {
+
+	// TODO: Mock-Up erstellen
+	
+}
---
+++ SS21_Uebung2/src/exercise2/test/AddressBookViewMockUp.java
@@ -0,0 +1,29 @@
+/*
+ * Example code used in exercises for lecture "Grundlagen des Software-Testens"
+ * Created and given by Ina Schieferdecker, Theo Vassiliou and Diana Serbanescu
+ * Technische Universität Berlin
+ */
+package exercise2.test;
+
+import exercise2.addressbook.controller.AddressBookController;
+import exercise2.addressbook.model.AddressBookAccess;
+import exercise2.addressbook.view.AddressBookView;
+
+/**
+ * Uebung 2 - Komponenten und Integrationstest
+ * Mock-Up für den AddressBookView
+ * 
+ * Bitte Nummer der Gruppe eintragen:
+ * 0
+ * 
+ * Bitte Gruppenmitglieder eintragen:
+ * @author ...
+ * @author ...
+ * @author ...
+ * @author ...
+ */
+public class AddressBookViewMockUp implements AddressBookView {
+
+	// TODO: Mock-Up erstellen
+	
+}
